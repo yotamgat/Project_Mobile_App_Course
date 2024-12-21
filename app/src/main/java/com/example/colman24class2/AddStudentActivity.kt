61a9cd11c7260283d7c099be389064776c3d6031
Yotam Gat <yotamgat2@gmail.com>
2024-12-21 18:28:18 +0200

    Adding add new student logic

--- app/src/main/java/com/example/colman24class2/AddStudentActivity.kt
+++ app/src/main/java/com/example/colman24class2/AddStudentActivity.kt
@@ -1,6 +1,9 @@
 package com.example.colman24class2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,24 @@ class AddStudentActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        //-----DEFINE----
+        val saveButton: Button = findViewById(R.id.add_student_activity_save_button)
+        val cancelButton: Button = findViewById(R.id.add_student_activity_cancel_button)
+        val nameEditText: EditText = findViewById(R.id.add_student_activity_name_edit_text)
+        val idEditText: EditText = findViewById(R.id.add_student_activity_id_edit_text)
+        val savedMessageTextView: TextView = findViewById(R.id.add_student_activity_save_message_text_view)
+
+        //-----LISTENERS------
+        cancelButton.setOnClickListener{
+            finish()
+        }
+
+        saveButton.setOnClickListener{
+            savedMessageTextView.text ="Name: ${nameEditText.text}, ID: ${idEditText.text} is saved!!!"
+        }
+
+
     }
 }
