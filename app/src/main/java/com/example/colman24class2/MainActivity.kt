25726135fff5f0232de60690ca84c90ff107af34
Yotam Gat <yotamgat2@gmail.com>
2024-12-21 16:20:13 +0200

    init commit

---
+++ app/src/main/java/com/example/colman24class2/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.colman24class2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    // TODO: Step 1 - Add Student Button
+    // TODO: Step 2 - Navigate To AddStudentActivity
+    // TODO: Step 3 - Create AddStudentLayout
+    // TODO: Step 4 - Save Student
+}
